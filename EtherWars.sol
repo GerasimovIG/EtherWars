eea11c52d46a01a6b859a16f67cc137aaad13d58
CPrim <huan_aeroplan@mail.ru>
2017-12-19 18:07:48 +0300

    1

--- EtherWars.sol
+++ EtherWars.sol
@@ -1,23 +1,38 @@
 pragma solidity^ 0.4.0;
 
 contract EtherWars{
+/*********************ПЕРЕМЕННЫЕ********************************/
     address sender;
+    mapping (address => uint) playerBalances;
+    mapping (address => bool) warCondition;
     
+    
+    
+/********************ФУНКЦИИ****************************/    
     function EtherWars() payable public{
-        
+        /*Такой конструктор с PayAble необходимо иметь, чтобы контракт мог высылать эфиры*/
     }
     
-    function receiveEther () public payable{
+    function transferEther () public payable{
+        playerBalances[msg.sender] += msg.value;
+        
     }
     
     function getBalance() view public returns (uint){
         return this.balance;
     }
     
-    function transferEther () public {
-        msg.sender.transfer(50000000);
+    function receiveEther (uint _spendTreasury) public {
+        require  (playerBalances[msg.sender]>= _spendTreasury);
+        msg.sender.transfer(_spendTreasury);
     }
     
+    function attackEnemy(address _enemy) public view {
+        require( warCondition[_enemy] == false);
+        warCondition[_enemy] == true;
+        warCondition[msg.sender] == true;
+    }
     
+  
 }
 
